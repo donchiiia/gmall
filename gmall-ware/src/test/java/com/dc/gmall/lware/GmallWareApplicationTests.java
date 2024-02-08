3176d2752fefe6ee9238ca43bc34943367ee2639
donchi <anckszzz@gmail.com>
2024-02-08 14:55:15 +0800

    项目初始结构创建

---
+++ gmall-ware/src/test/java/com/dc/gmall/lware/GmallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dc.gmall.lware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
